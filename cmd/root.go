e62e843cfbde5036e256a24416668ff28ba15498
Mathias Andresen <mathias@3mogs.dk>
2025-03-13 10:16:37 +0100

    fix being able to use version flag

--- cmd/root.go
+++ cmd/root.go
@@ -3,13 +3,17 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
+var version = "0.1.0"
+
 var rootCmd = &cobra.Command{
-	Use:   "gitc",
-	Short: "A tool to switch between Git contexts",
+	Use:     "gitc",
+	Short:   "A tool to switch between Git contexts",
+	Version: version,
 	Run: func(cmd *cobra.Command, args []string) {
 		// If no arguments provided, show the list of contexts
 		if len(args) == 0 {
@@ -24,9 +28,9 @@ var rootCmd = &cobra.Command{
 func Execute() {
 	// Check for any args before executing cobra
 	if len(os.Args) > 1 {
-		// Skip if the first argument is a known command
+		// Skip if the first argument is a known command or flag
 		firstArg := os.Args[1]
-		if firstArg != "init" && firstArg != "list" && firstArg != "switch" {
+		if firstArg != "init" && firstArg != "list" && firstArg != "switch" && !strings.HasPrefix(firstArg, "-") {
 			// Insert "switch" as the first argument
 			newArgs := make([]string, len(os.Args)+1)
 			newArgs[0] = os.Args[0]
